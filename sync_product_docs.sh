e26cb9ab3ad82bb71936399badd905b3c1a279c6
Kyle Ellrott <kellrott@gmail.com>
2026-01-13 22:04:35 -0800

    Starting to working on doc sync tool

--- sync_product_docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/bmeg/grip.git products/grip
-git clone https://github.com/ohsu-comp-bio/funnel.git products/funnel
-git clone https://github.com/calypr/git-drs.git products/git-drs
-
-rsync -av products/grip/website/content/docs/ ./docs/tools/grip
-rsync -av products/funnel/website/content/ ./docs/tools/funnel
-rsync -av products/git-drs/docs/ ./docs/tools/git-drs
